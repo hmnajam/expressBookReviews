2ae57f4011c194753b861c199fb3e2a049241195
Najam <hmnajam@gmail.com>
2023-12-11 21:10:23 +0500

    tasks till 5 done and no bugs

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,64 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  1: {
+    isbn: "9780385474542",
+    author: "Chinua Achebe",
+    title: "Things Fall Apart",
+    reviews: { 1: "This is a good book" },
+  },
+  2: {
+    isbn: "9780143039969",
+    author: "Hans Christian Andersen",
+    title: "Fairy tales",
+    reviews: { 2: "Great collection of fairy tales" },
+  },
+  3: {
+    isbn: "9780142437223",
+    author: "Dante Alighieri",
+    title: "The Divine Comedy",
+    reviews: { 3: "A masterpiece of world literature" },
+  },
+  4: {
+    isbn: "9780140441000",
+    author: "Unknown",
+    title: "The Epic Of Gilgamesh",
+    reviews: { 4: "An ancient epic with profound themes" },
+  },
+  5: {
+    isbn: "9780192802979",
+    author: "Unknown",
+    title: "The Book Of Job",
+    reviews: { 5: "Philosophical and thought-provoking" },
+  },
+  6: {
+    isbn: "9780140441000",
+    author: "Unknown",
+    title: "One Thousand and One Nights",
+    reviews: { 6: "Captivating stories of the Arabian Nights" },
+  },
+  7: {
+    isbn: "9780140441000",
+    author: "Unknown",
+    title: "Nj\u00e1l's Saga",
+    reviews: { 7: "Classic Icelandic saga" },
+  },
+  8: {
+    isbn: "9780141439518",
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    reviews: { 8: "A delightful romantic novel" },
+  },
+  9: {
+    isbn: "9780140441000",
+    author: "Honor\u00e9 de Balzac",
+    title: "Le P\u00e8re Goriot",
+    reviews: { 9: "A compelling portrayal of Parisian society" },
+  },
+  10: {
+    isbn: "9780802144345",
+    author: "Samuel Beckett",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    reviews: { 10: "Existential exploration in the form of a trilogy" },
+  },
+};
 
-module.exports=books;
+module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,77 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get("/", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find((book) => book.isbn === isbn);
+
+  if (book) {
+    return res.json(book);
+  } else {
+    // If the ISBN is not found
+    res.status(404).json({ error: "Book not found" });
+  }
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get("/author/:author", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase()
+  );
+
+  if (booksByAuthor.length > 0) {
+    return res.json(booksByAuthor);
+  } else {
+    // If the author is not found
+    res.status(404).json({ error: "Books not found for the specified author" });
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const book = Object.values(books).find((book) => book.title === title);
+  if (book) {
+    return res.json(book);
+  } else {
+    // If the title is not found
+    res.status(404).json({ error: "Book not found" });
+  }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find((book) => book.isbn === isbn);
+
+  // Check if the book with the given ISBN exists
+  if (book) {
+    const reviews = book.reviews || {};
+    return res.json(reviews);
+  } else {
+    // If the book with the given ISBN is not found
+    res.status(404).json({ error: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
